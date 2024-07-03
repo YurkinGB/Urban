7af56a771dc9b4d4e6d67ed7c7445e7e8da026a9
YurkinGB <79604951+YurkinGB@users.noreply.github.com>
2024-07-04 00:52:55 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -3,21 +3,23 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 prime = []
 not_prime = []
 prime_not_prime_dict = {}
-
-numbers.remove(1)
+is_prime = True
 
 for i in numbers:
-    is_prime = True
+    if i != 1:
+        prime_not_prime_dict.update({i: is_prime})
+
+for i in list(prime_not_prime_dict.keys()):
     for j in range(2, i):
         if i % j == 0:
-            is_prime = False
-            prime_not_prime_dict.update({i: is_prime})
+            prime_not_prime_dict.update({i: False})
             break
 
-for i in numbers:
-    if i != 1 and i not in list(prime_not_prime_dict.keys()):
+for i in list(prime_not_prime_dict.keys()):
+    if prime_not_prime_dict[i] == is_prime:
         prime.append(i)
     else:
         not_prime.append(i)
+
 print('Prime: ', prime)
 print('Non prime: ', not_prime)
