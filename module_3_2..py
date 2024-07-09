e4b2ab556f931d8b8889edfb9590f516d86ffa24
YurkinGB <79604951+YurkinGB@users.noreply.github.com>
2024-07-09 21:27:02 +0300

    Update module_3_2..py

--- module_3_2..py
+++ module_3_2..py
@@ -1,6 +1,6 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
-    if (not recipient.endswith((".com", ".ru", ".net")) or not (
-            not ('@' not in recipient) and sender.endswith((".com", ".ru", ".net"))) or '@' not in sender):
+    if (not recipient.endswith((".com", ".ru", ".net")) or '@' not in recipient
+            and not sender.endswith((".com", ".ru", ".net")) or '@' not in sender):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
@@ -10,7 +10,7 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
 
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Это сообщение для проверки связи', 'vasyok1337@.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
