16cf2cbdaa783e6f9cbcdcfdf69d01540f301b83
YurkinGB <79604951+YurkinGB@users.noreply.github.com>
2024-07-17 00:29:29 +0300

    Update module3hard.py

--- module3hard.py
+++ module3hard.py
@@ -1,9 +1,9 @@
 data_structure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
 sum = 0
@@ -23,10 +23,5 @@ def calculate_structure_sum(*args):
     return sum
 
 
-
-
-
-
-
-#result = calculate_structure_sum(*ata_structure)
-print(calculate_structure_sum(data_structure))
+result = calculate_structure_sum(data_structure)
+print(result)
