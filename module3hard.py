0d29315e6a88cbc9eb0cc1dec8bf1e2136329f85
YurkinGB <79604951+YurkinGB@users.noreply.github.com>
2024-07-17 00:28:09 +0300

    Add files via upload

---
+++ module3hard.py
@@ -0,0 +1,32 @@
+data_structure = [
+  [1, 2, 3],
+  {'a': 4, 'b': 5},
+  (6, {'cube': 7, 'drum': 8}),
+  "Hello",
+  ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+sum = 0
+
+
+def calculate_structure_sum(*args):
+    global sum
+    for arg in args:
+        if isinstance(arg, int):
+            sum += arg
+        elif isinstance(arg, str):
+            sum += len(arg)
+        elif isinstance(arg, dict):
+            calculate_structure_sum(*arg.items())
+        else:
+            calculate_structure_sum(*arg)
+    return sum
+
+
+
+
+
+
+
+#result = calculate_structure_sum(*ata_structure)
+print(calculate_structure_sum(data_structure))
