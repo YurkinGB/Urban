c5048ddfc52e114ca7a46949738feb48a7794b33
YurkinGB <79604951+YurkinGB@users.noreply.github.com>
2024-08-25 21:46:57 +0300

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+directory = r'C:\Users\aliga\PycharmProjects\Urban\test'
+
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(directory, file)
+        filetime = os.path.getmtime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(filepath)
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
+          f'Родительская директория: {parent_dir}')
