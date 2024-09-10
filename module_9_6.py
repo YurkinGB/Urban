29ff3fc1070b4ff3f2a0e554e9ddcc1b51aeaff4
YurkinGB <79604951+YurkinGB@users.noreply.github.com>
2024-09-11 01:37:18 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,7 +1,11 @@
-def all_variants(text):
-    for i in text:
-        yield i
+from itertools import product
 
+
+def all_variants(text):
+    for i in range(1, len(text) + 1):
+        for ch in product(text, repeat=i):
+            if "".join(ch) in text:
+                yield "".join(ch)
 
 a = all_variants("abc")
 for i in a:
